e864e8a7f0592986dff2d3dfdadb2ea4526fa205
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2021-04-01 13:34:11 +0100

    Can display a list of clients first names

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import {Routes, RouterModule} from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ClientsComponent } from './home/clients/clients.component'
 import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    ClientsComponent,
     AuthComponent
   ],
   imports: [
